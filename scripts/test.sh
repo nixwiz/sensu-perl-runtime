15e5ec0d70ac268427f9f96f4a0acf04ba8ae04f
Todd Campbell <todd@sensu.io>
2020-01-16 07:54:38 -0800

    Workaround (in an ugly fashion) the fact that OEL images may not have tar installed
    Signed-off-by: Todd Campbell <todd@sensu.io>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+install_tar() {
+  # Please don't look at me, I'm hideous
+  if type yum > /dev/null 2>&1; then
+    yum -y install tar
+  elif type apt-get > /dev/null 2>&1; then
+    apt-get install -y tar
+  elif type apk > /dev/null 2>&1; then
+    apk --no-cache add tar
+  fi
+}
+
 echo "Test Script:"
 echo "  Asset Platform:  ${platform}"
 echo "  Target Platform: ${test_platform}"
@@ -9,6 +21,11 @@ if [ -z "$asset_filename" ]; then
 fi
 mkdir -p /build
 cd /build
+# Some Docker images (I'm talking to you Oracle Linux) don't
+# include tar, so go find it
+if [ ! -x /bin/tar ] && [ ! -x /usr/bin/tar ]; then
+  install_tar
+fi
 tar xzf /dist/$asset_filename
 LD_LIBRARY_PATH="/build/lib:$LD_LIBRARY_PATH" /build/bin/perl /scripts/test_ssl_url.pl
 
